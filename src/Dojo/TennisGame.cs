92c3b84930acd28d430da26ad751a520e8c77bbe
Peter Sacchet <webmaster@c-designed.com>
2010-09-01 19:19:18 +0200

    autocommit after successful build

---
+++ src/Dojo/TennisGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class TennisGameTests
+    {
+        [Test]
+        public void When_Each_Player_Have_No_Points_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+            bool isOver = game.IsGameOver;
+            Assert.That(isOver, Is.False);
+        }
+    }
+
+    public class TennisGame
+    {
+        public bool IsGameOver { get; set; }
+    }
+}
